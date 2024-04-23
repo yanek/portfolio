58a7873ed912b3387278b8dd1735d6356194b70d
Noé Ksiazek <noe@ksiazek.xyz>
2024-04-23 16:51:11 +0200

    Parse HTML in markdown, to enable target=_blank

--- src/components/LocalizedText.jsx
+++ src/components/LocalizedText.jsx
@@ -2,18 +2,23 @@ import { Fragment, useContext } from 'react';
 import Markdown from 'react-markdown';
 import { LocaleContext } from './App';
 import { PropTypes } from 'prop-types';
+import rehypeRaw from 'rehype-raw';
 
 LocalizedText.propTypes = {
-  textId: PropTypes.string
+  textId: PropTypes.string,
 };
 
 /**
- * @param {{textId: string}} props 
+ * @param {{textId: string}} props
  * @returns {ReactElement}
  */
-function LocalizedText({textId}) {
+function LocalizedText({ textId }) {
   const ctx = useContext(LocaleContext);
-  return <Markdown components={{p: Fragment}}>{ctx.locale.getText(textId)}</Markdown>;
+  return (
+    <Markdown components={{ p: Fragment }} rehypePlugins={[rehypeRaw]}>
+      {ctx.locale.getText(textId)}
+    </Markdown>
+  );
 }
 
 export default LocalizedText;
