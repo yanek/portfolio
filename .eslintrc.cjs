28ac14a85496751ad88afcc6ad2a364a6cd5a230
Noé Ksiazek <noe@ksiazek.xyz>
2024-04-24 11:25:41 +0200

    Setup for TypeScript migration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,29 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
-  extends: ['eslint:recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   settings: {
     react: {
-      version: '18.2',
+      version: "18.2",
     },
   },
-  plugins: ['react-refresh'],
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
   },
 };
