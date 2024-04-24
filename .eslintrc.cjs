cf06ff755e0421bfceaf2f099ac41c880f478007
Noé Ksiazek <noe@ksiazek.xyz>
2024-04-24 10:51:05 +0200

    Enable node env in ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: ['eslint:recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: {
